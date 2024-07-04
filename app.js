f912e5075fad404d933f604d278ec17f67910a91
alan benavente <alanbenavente02@gmail.com>
2024-07-03 23:04:40 -0300

    proyecto 1a

--- app.js
+++ app.js
@@ -5,35 +5,53 @@
 /* let  nombre = valor
 const dni = valor */
 
-let apellido = "perez";
-let nombre = "juan";
+alert("Hola! " + "Bienvenido a CineStar");
+
+alert("Por favor, inicie sesion para comprar las entradas");
+
+let usuario = prompt("Ingrese su nombre de usuario");
+
+let clave = prompt("Ingrese su clave");
 
 // alert (nombre);
 
 // alert (apellido);
 
-console.log(nombre);
-// console.log(apellido);
+console.log(usuario);
 
-const documento = "11223344"; // ---> number
+alert("Gracias por iniciar sesion!");
 
-nombre = "juancito";
+alert("Elija la pelicula");
 
-console.log(nombre);
+let p1 = "Intensamente 2";
 
-let edad_de_la_persona = 40;
+let p2 = "Terminator 1";
 
-console.log(edad_de_la_persona);
+let peliculas = prompt("Intensamente 2 ó Terminator 1");
+
+console.log(peliculas);
+
+let cantidadDeEntradas = Number(
+  prompt("Seleccione la cantidad de entradas (200 pesos c/u)")
+);
+let precioDeEntrada = Number(200);
+console.log(cantidadDeEntradas);
+
+let totalAPagar = cantidadDeEntradas * precioDeEntrada;
+
+alert("Su total es de " + totalAPagar);
+
+console.log(totalAPagar);
 
 // let numero = prompt("por favor ingresar numero");
 
 // console.log(numero);
 
-//let nombreDeUsuario = prompt("por favor inicia secion con tu nombre de usuario")
+//let nombreDeUsuario = prompt("por favor inicia sesion con tu nombre de usuario")
 
 //alert("hola " + nombreDeUsuario + " como estas?");
 
-let n1 = Number(prompt("ingresa el primer numero"));
+/* let n1 = Number(prompt("ingresa el primer numero"));
 let n2 = Number(prompt("ingresa el segundo numero"));
 
 let total = n1 + n2;
@@ -42,4 +60,8 @@ console.log(total);
 
 // boolean --> booleano 0/1 si o no true o false
 
-console.log(n1 > n2);
+console.log(n1 > n2); */
+
+/* if(la condicion) {
+lo que quiero hacer
+} */
