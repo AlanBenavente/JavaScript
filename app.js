79f38d552ce0ff0ad47199692d98981db8f85a30
alan benavente <alanbenavente02@gmail.com>
2024-07-03 22:00:32 -0300

    proyecto

---
+++ app.js
@@ -0,0 +1,45 @@
+// variables y tipo de datos primitivos
+
+// let o const
+
+/* let  nombre = valor
+const dni = valor */
+
+let apellido = "perez";
+let nombre = "juan";
+
+// alert (nombre);
+
+// alert (apellido);
+
+console.log(nombre);
+// console.log(apellido);
+
+const documento = "11223344"; // ---> number
+
+nombre = "juancito";
+
+console.log(nombre);
+
+let edad_de_la_persona = 40;
+
+console.log(edad_de_la_persona);
+
+// let numero = prompt("por favor ingresar numero");
+
+// console.log(numero);
+
+//let nombreDeUsuario = prompt("por favor inicia secion con tu nombre de usuario")
+
+//alert("hola " + nombreDeUsuario + " como estas?");
+
+let n1 = Number(prompt("ingresa el primer numero"));
+let n2 = Number(prompt("ingresa el segundo numero"));
+
+let total = n1 + n2;
+
+console.log(total);
+
+// boolean --> booleano 0/1 si o no true o false
+
+console.log(n1 > n2);
